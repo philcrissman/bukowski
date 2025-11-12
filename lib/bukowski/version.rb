f39301091c54b1d5af3f68257b6cd4e7a611dee0
Phil Crissman <phil.crissman@gmail.com>
2025-11-11 19:09:01 -0800

    First commit

---
+++ lib/bukowski/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Bukowski
+  VERSION = "0.1.0"
+end
---
+++ test/test_bukowski.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestBukowski < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Bukowski::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
